0c41dc871d3ae5cc6b74fa0296acaf27f7116764
gdormoy <guillaume.dormoy@disney.com>
2019-12-23 07:55:13 -0800

    use function in utils for test

--- Bonus/main.py
+++ Bonus/main.py
@@ -15,52 +15,17 @@ if __name__ == "__main__":
     f.close()
     key56bit = convert_64bit_to_56bit_key(key)
     cp1 = Constantes['CP_1'][0]
+    PI = Constantes['PI'][0]
     print(cp1)
-    test = "01011110101101010100101111110101000000110110111101001000"
+    test = "0101111001011011010100100111111101010001000110101011110010010001"
 
     keyPermutation0 = permutation(cp1,test)
     print(f'Key:\n{test}')
     print(f'KeyPermutation:\n{keyPermutation0}')
-    # print()
-    # print(key56bit)
-
-    # for x in range(len(key56bit)):
-    #     print(key56bit[x], end = "\t")
-    #     if (x + 1) % 8 == 0:
-    #         print()
-
+    cutingKey = cutKey(keyPermutation0)
+    print(cutingKey)
+    cutingKey[0] = leftShift(cutingKey[0])
+    print(cutingKey)
     bintxt = conv_bin(newtxt)
 
     cuttedBinTxt = cutBinText(bintxt)
-    # print(f'cuttedBinTxt 0\n{cuttedBinTxt[0]}')
-
-    # for x in range(len(cuttedBinTxt)):
-    #     for y in range(len(cuttedBinTxt[x])):
-    #         print(cuttedBinTxt[x][y], end = '\t')
-    #     print()
-    # print()
-
-    PI = Constantes['PI'][0]
-
-    # print(f'PI\n{PI}')
-    p0 = []
-    # for x in range(len(cuttedBinTxt)):
-    #     p0.append(permutation(PI,cuttedBinTxt[x]))
-    #
-    # for x in range(len(p0)):
-    #     print(f'p0 {x}\n{p0[x]}')
-
-    # for x in range(len(PI)):
-    #     print(PI[x], end = '\t')
-    # print()
-
-
-    # print("\nPI:")
-    # count = 1
-    # for value in range(len(Constantes['PI'][0])):
-    #     print(Constantes['PI'][0][value], end = "\t")
-    #     if (value + 1) % 8 == 0:
-    #         print()
-
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(Constantes)
