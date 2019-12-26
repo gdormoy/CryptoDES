a2013e1557ea210256959914bfcb72d4fce202b0
gdormoy <guillaume.dormoy@disney.com>
2019-12-26 05:06:34 -0800

    add encryption function

--- Bonus/main.py
+++ Bonus/main.py
@@ -3,6 +3,15 @@ from ConvAlphaBin import *
 from utils import *
 from pprint import pprint
 
+def encryption(key,txt):
+    bintxt = conv_bin(txt)
+    k = createKeys(key,Constantes)
+    m = permuteText(bintxt,Constantes)
+    r = ronde(k,m,Constantes)
+    encryption = ''.join(r.values())
+    print(nib_vnoc(encryption))
+    return encryption
+
 if __name__ == "__main__":
     Constantes = recupConstantesDES()
     f=open("../Messages/test.txt", "r")
@@ -13,23 +22,4 @@ if __name__ == "__main__":
     f=open("../Messages/Clef_de_1.txt", "r")
     key=f.read()
     f.close()
-    k = dict()
-    m = dict()
-    # cp1 = Constantes['CP_1'][0]
-    # PI = Constantes['PI'][0]
-    test = "0101111001011011010100100111111101010001000110101011110010010001"
-    testbintxt= '1101110010111011110001001101010111100110111101111100001000110010100111010010101101101011111000110011101011011111'
-    k = createKeys(test,Constantes)
-    bintxt = conv_bin(newtxt)
-    cuttedBinTxt = cutBinText(testbintxt)
-    pprint(cuttedBinTxt)
-
-    for x in range(len(cuttedBinTxt)):
-        m[x] = permutation(Constantes['PI'][0], cuttedBinTxt[x])
-
-    pprint(m)
-
-    cuttingKey = cutKey(m[0])
-    print(cuttingKey)
-    # cuttingKey[0] = leftShift(cutingKey[0])
-    # cuttingKey[1] = leftShift(cutingKey[1])
+    encryption(key,newtxt)
