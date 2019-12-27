2ccec3f95a395d68203e9ed99980b3ab4026fb7a
gdormoy <guillaume.dormoy@disney.com>
2019-12-27 07:04:24 -0800

    update encryption and decryption

--- Bonus/main.py
+++ Bonus/main.py
@@ -9,15 +9,29 @@ def encryption(key,txt):
     k = createKeys(key,Constantes)
     m = permuteText(bintxt,Constantes)
     r = ronde(k,m,Constantes)
-    encryption = ''.join(r.values())
-    res = nib_vnoc(encryption)
+    # encryption = ''.join(r.values())
+    res = nib_vnoc(r)
+    print(r)
     return {'binary': encryption, 'text': res}
 
 def decryption(key,txt):
     print("DECRYPTION")
     decrypt = ''
     res = ''
+    # bintxt = conv_bin(txt)
+    bintxt = '1000100000110110101000010001001111001011011000001001010010010000'
+    # print(txt)
+    # print(bintxt)
+    k = createKeys(key,Constantes)
+    r = decryptRonde(k,bintxt,Constantes)
+    # m = permuteText(bintxt,Constantes)
+    decrypt = ''.join(r.values())
+    res = nib_vnoc(decrypt)
+    print(decrypt)
+    print(res)
     return {'binary': decrypt, 'text': res}
+
+
 
 if __name__ == "__main__":
     command = sys.argv[1]
@@ -26,14 +40,14 @@ if __name__ == "__main__":
     res = dict()
 
     Constantes = recupConstantesDES()
-    f=open(pathToText, "r")
-    txt=f.read()
-    f.close()
+    with open(pathToText, 'r') as file:
+        txt = file.read()
+    txt = txt.rstrip()
     newtxt = FiltreTXT(txt)
 
-    f=open(pathToKey, "r")
-    key=f.read()
-    f.close()
+    with open(pathToKey, 'r') as file:
+        key = file.read()
+    key = key.rstrip('\n')
 
     if command.upper() == "ENCRYPT":
         res = encryption(key,newtxt)
