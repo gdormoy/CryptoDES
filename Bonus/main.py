f7a84d1cc8cc0dcba4966cb479b2ba28774fcf75
gdormoy <guillaume.dormoy@disney.com>
2019-12-30 07:48:13 -0800

    remove useless code

--- Bonus/main.py
+++ Bonus/main.py
@@ -5,11 +5,11 @@ from utils import *
 from pprint import pprint
 
 def encryption(key,txt):
-    bintxt = conv_bin(txt)
+    newtxt = FiltreTXT(txt)
+    bintxt = conv_bin(newtxt)
     k = createKeys(key,Constantes)
     m = permuteText(bintxt,Constantes)
     r = ronde(k,m,Constantes)
-    # encryption = ''.join(r.values())
     res = nib_vnoc(r)
     print(r)
     return {'binary': encryption, 'text': res}
@@ -20,18 +20,11 @@ def decryption(key,txt):
     res = ''
     bintxt = conv_bin(txt)
     # bintxt = '1000100000110110101000010001001111001011011000001001010010010000'
-    # print(txt)
-    # print(bintxt)
     k = createKeys(key,Constantes)
     m = permuteText(bintxt,Constantes)
-    # print('m', m)
     r = decryptRonde(k,m,Constantes)
-    print(r)
 
-    # decrypt = ''.join(r.values())
     res = nib_vnoc(r)
-    # print(decrypt)
-    print(res)
     return {'binary': decrypt, 'text': res}
 
 
@@ -46,14 +39,13 @@ if __name__ == "__main__":
     with open(pathToText, 'r') as file:
         txt = file.read()
     txt = txt.rstrip()
-    newtxt = FiltreTXT(txt)
 
     with open(pathToKey, 'r') as file:
         key = file.read()
     key = key.rstrip('\n')
 
     if command.upper() == "ENCRYPT":
-        res = encryption(key,newtxt)
+        res = encryption(key,txt)
     elif command.upper() == "DECRYPT":
         res = decryption(key,txt)
 
